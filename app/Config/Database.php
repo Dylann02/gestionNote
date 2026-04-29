2978322d0db0d96b2b57ba8e19b98d6bfeadff28
Dylann02 <hopkinslucka@gmail.com>
2026-04-29 10:20:12 +0300

    changement base et script

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => '127.0.0.1',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'produits',
+        'database'     => 'tp_gestion_notes',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
